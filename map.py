0920212188330a795c98a393c30f439071f22087
Jean-Luc Chamaa <jlchamaa@gmail.com>
2018-01-04 02:39:48 +0000

    functional prototype

---
+++ map.py
@@ -0,0 +1,132 @@
+teams = {
+'Baltimore Orioles': {'lednum': 37, 'color1': '#000000','color2': '#000000'},
+'Boston Red Sox': {'lednum': 40, 'color1': '#000000','color2': '#000000'},
+'New York Yankees': {'lednum': 39, 'color1': '#000000','color2': '#000000'},
+'Tampa Bay Rays': {'lednum': 15, 'color1': '#000000','color2': '#000000'},
+'Toronto Blue Jays': {'lednum': 27, 'color1': '#000000','color2': '#000000'},
+'Atlanta Braves': {'lednum': 19, 'color1': '#000000','color2': '#000000'},
+'Miami Marlins': {'lednum': 16, 'color1': '#000000','color2': '#000000'},
+'New York Mets': {'lednum': 39, 'color1': '#000000','color2': '#000000'},
+'Philadelphia Phillies': {'lednum': 38, 'color1': '#000000','color2': '#000000'},
+'Washington Nationals': {'lednum': 36, 'color1': '#000000','color2': '#000000'},
+'Chicago White Sox': {'lednum': 25, 'color1': '#000000','color2': '#000000'},
+'Cleveland Indians': {'lednum': 29, 'color1': '#000000','color2': '#000000'},
+'Detroit Tigers': {'lednum': 26, 'color1': '#000000','color2': '#000000'},
+'Kansas City Royals': {'lednum': 21, 'color1': '#000000','color2': '#000000'},
+'Minnesota Twins': {'lednum': 22, 'color1': '#000000','color2': '#000000'},
+'Chicago Cubs': {'lednum': 25, 'color1': '#000000','color2': '#000000'},
+'Cincinnati Reds': {'lednum': 34, 'color1': '#000000','color2': '#000000'},
+'Milwaukee Brewers': {'lednum': 24, 'color1': '#000000','color2': '#000000'},
+'Pittsburgh Pirates': {'lednum': 35, 'color1': '#000000','color2': '#000000'},
+'St. Louis Cardinals': {'lednum': 31, 'color1': '#000000','color2': '#000000'},
+'Houston Astros': {'lednum': 13, 'color1': '#000000','color2': '#000000'},
+'Los Angeles Angels': {'lednum': 3, 'color1': '#000000','color2': '#000000'},
+'Oakland Athletics': {'lednum': 4, 'color1': '#000000','color2': '#000000'},
+'Seattle Mariners': {'lednum': 7, 'color1': '#000000','color2': '#000000'},
+'Texas Rangers': {'lednum': 11, 'color1': '#000000','color2': '#000000'},
+'Arizona Diamondbacks': {'lednum': 2, 'color1': '#000000','color2': '#000000'},
+'Colorado Rockies': {'lednum': 9, 'color1': '#000000','color2': '#000000'},
+'Los Angeles Dodgers': {'lednum': 3, 'color1': '#000000','color2': '#000000'},
+'San Diego Padres': {'lednum': 1, 'color1': '#000000','color2': '#000000'},
+'San Francisco Giants': {'lednum': 4, 'color1': '#000000','color2': '#000000'},
+'Boston Celtics': {'lednum': 40, 'color1': '#008248','color2': '#BA9653'},
+'Brooklyn Nets': {'lednum': 39, 'color1': '#FFFFFF','color2': '#000000'},
+'New York Knicks': {'lednum': 39, 'color1': '#006BB6','color2': '#f58426'},
+'Philadelphia 76ers': {'lednum': 38, 'color1': '#006BB6','color2': '#ED174C'},
+'Toronto Raptors': {'lednum': 27, 'color1': '#CD1141','color2': '#A0A0A3'},
+'Golden State Warriors': {'lednum': 4, 'color1': '#243E90','color2': '#FFCD34'},
+'LA Clippers': {'lednum': 3, 'color1': '#ED174C','color2': '#006BB6'},
+'Los Angeles Lakers': {'lednum': 3, 'color1': '#552583','color2': '#FDB927'},
+'Phoenix Suns': {'lednum': 2, 'color1': '#E56020','color2': '#1D1160'},
+'Sacramento Kings': {'lednum': 5, 'color1': '#5A2D81','color2': '#63727A'},
+'Chicago Bulls': {'lednum': 25, 'color1': '#CE1141','color2': '#FFFFFF'},
+'Cleveland Cavaliers': {'lednum': 29, 'color1': '#6F2633','color2': '#FFB81C'},
+'Detroit Pistons': {'lednum': 26, 'color1': '#ED174C','color2': '#0067B1'},
+'Indiana Pacers': {'lednum': 30, 'color1': '#002D62','color2': '#FDBB30'},
+'Milwaukee Bucks': {'lednum': 24, 'color1': '#00471B','color2': '#EEE1C6'},
+'Dallas Mavericks': {'lednum': 11, 'color1': '#007DC5','color2': '#C4CED4'},
+'Houston Rockets': {'lednum': 13, 'color1': '#CE1141','color2': '#FFFFFF'},
+'Memphis Grizzlies': {'lednum': 20, 'color1': '#00285E','color2': '#6189B9'},
+'New Orleans Pelicans': {'lednum': 14, 'color1': '#002B5C','color2': '#002B5C'},
+'San Antonio Spurs': {'lednum': 12, 'color1': '#C4CED4','color2': '#FFFFFF'},
+'Atlanta Hawks': {'lednum': 19, 'color1': '#E03A3E','color2': '#FFFFFF'},
+'Charlotte Hornets': {'lednum': 33, 'color1': '#1D1160','color2': '#00788C'},
+'Miami Heat': {'lednum': 16, 'color1': '#98002E','color2': '#FFFFFF'},
+'Orlando Magic': {'lednum': 17, 'color1': '#0B77BD','color2': '#C2CCD2'},
+'Washington Wizards': {'lednum': 36, 'color1': '#002B5C','color2': '#E31837'},
+'Denver Nuggets': {'lednum': 9, 'color1': '#5091CD','color2': '#FDB927'},
+'Minnesota Timberwolves': {'lednum': 22, 'color1': '#002B5C','color2': '#7AC143'},
+'Oklahoma City Thunder': {'lednum': 10, 'color1': '#007AC1','color2': '#F05133'},
+'Portland Trail Blazers': {'lednum': 6, 'color1': '#E13A3E','color2': '#C4CED4'},
+'Utah Jazz': {'lednum': 8, 'color1': '#0C2340','color2': '#00471B'},
+'Dallas Cowboys': {'lednum': 11, 'color1': '#041E42','color2': '#FFFFFF'},
+'New York Giants': {'lednum': 39, 'color1': '#001E62','color2': '#A6192E'},
+'Philadelphia Eagles': {'lednum': 38, 'color1': '#064C53','color2': '#70808F'},
+'Washington Redskins': {'lednum': 36, 'color1': '#862633','color2': '#FFCD00'},
+'Buffalo Bills': {'lednum': 28, 'color1': '#00338D','color2': '#C8102E'},
+'Miami Dolphins': {'lednum': 16, 'color1': '#008E97','color2': '#F58220'},
+'New England Patriots': {'lednum': 40, 'color1': '#0C2340','color2': '#C8102E'},
+'New York Jets': {'lednum': 39, 'color1': '#0C371D','color2': '#FFFFFF'},
+'Arizona Cardinals': {'lednum': 2, 'color1': '#97233F','color2': '#FFC20E'},
+'Los Angeles Rams': {'lednum': 3, 'color1': '#002244','color2': '#FFFFFF'},
+'San Francisco 49ers': {'lednum': 4, 'color1': '#AA0000','color2': '#B3995E'},
+'Seattle Seahawks': {'lednum': 7, 'color1': '#001433','color2': '#4DFF00'},
+'Denver Broncos': {'lednum': 9, 'color1': '#FC4C02','color2': '#0C2340'},
+'Kansas City Chiefs': {'lednum': 21, 'color1': '#C8102E','color2': '#FFB81C'},
+'Los Angeles Chargers': {'lednum': 3, 'color1': '#0072CE','color2': '#FFB81C'},
+'Oakland Raiders': {'lednum': 4, 'color1': '#101820','color2': '#A5ACAF'},
+'Chicago Bears': {'lednum': 25, 'color1': '#051C2C','color2': '#DC4405'},
+'Detroit Lions': {'lednum': 26, 'color1': '#0069B1','color2': '#FFFFFF'},
+'Green Bay Packers': {'lednum': 23, 'color1': '#175E33','color2': '#FFB81C'},
+'Minnesota Vikings': {'lednum': 22, 'color1': '#4F2683','color2': '#FFC62F'},
+'Baltimore Ravens': {'lednum': 37, 'color1': '#241773','color2': '#D0B240'},
+'Cincinnati Bengals': {'lednum': 34, 'color1': '#FC4C02','color2': '#FC4C02'},
+'Cleveland Browns': {'lednum': 29, 'color1': '#EB3300','color2': '#382F2D'},
+'Pittsburgh Steelers': {'lednum': 35, 'color1': '#FFB81C','color2': '#A5ACAF'},
+'Atlanta Falcons': {'lednum': 19, 'color1': '#A6192E','color2': '#8C8B89'},
+'Carolina Panthers': {'lednum': 33, 'color1': '#0085CA','color2': '#A5ACAF'},
+'New Orleans Saints': {'lednum': 14, 'color1': '#D3BC8D','color2': '#FFFFFF'},
+'Tampa Bay Buccaneers': {'lednum': 15, 'color1': '#C8102E','color2': '#FF8200'},
+'Houston Texans': {'lednum': 13, 'color1': '#091F2C','color2': '#A6192E'},
+'Indianapolis Colts': {'lednum': 30, 'color1': '#003A70','color2': '#FFFFFF'},
+'Jacksonville Jaguars': {'lednum': 18, 'color1': '#006073','color2': '#D49F12'},
+'Tennessee Titans ': {'lednum': 32, 'color1': '#0C2340','color2': '#4B92DB'} };
+import subprocess
+import serial, time, datetime
+
+def execute(cmd):
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line 
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+
+ser = serial.Serial('/dev/ttyACM0', 38400)
+
+def application(team,points):
+    points=int(points)
+    cityNum=int(teams[team]['lednum'])
+    temp=teams[team]['color1']
+    col1r=int(temp[1:3],16)
+    col1g=int(temp[3:5],16)
+    col1b=int(temp[5:7],16)
+    temp=teams[team]['color2']
+    col2r=int(temp[1:3],16)
+    col2g=int(temp[3:5],16)
+    col2b=int(temp[5:7],16)
+    print(cityNum,points,col1r,col1g,col1b,col2r,col2g,col2b)
+    ba=bytearray()
+    ba[0:8]=[cityNum,points,col1r,col1g,col1b,col2r,col2g,col2b,0]
+    for index,value in enumerate(ba):
+        ba[index]=min(255,value+1) #ensures zerobyte is the sole zero.  Adjust values back on Arduino Side!
+    ba[8]=int(0)
+    print ser.write(ba)
+
+for path in execute(["node", "sockets.js"]):
+    plusPos = path.find('+')
+    team=path[0:plusPos]
+    points = int(path[plusPos+1:])
+    application(team,points)
+    print path
