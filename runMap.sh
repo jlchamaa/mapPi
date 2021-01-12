57c4443ad1a3fafc5c9687ecbb1dd702f4c7c4ba
Jean-Luc <jlchamaa@gmail.com>
2021-01-11 16:42:49 -0800

    Remove outdated crap

--- runMap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/usr/bin/tmux send-keys -t "0:1.1" C-C C-L " cd /home/jlc/mapPi" C-m " ./map.py mlb" C-m
-/usr/bin/tmux send-keys -t "0:1.2" C-C C-L " cd /home/jlc/mapPi" C-m " ./map.py nfl" C-m 
-/usr/bin/tmux send-keys -t "0:1.3" C-C C-L " cd /home/jlc/mapPi" C-m " ./map.py nba" C-m
