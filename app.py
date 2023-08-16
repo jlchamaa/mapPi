963f34fe8fb4ede129aa645ddd0f8aba1beea407
Jean-Luc Chamaa <jlchamaa@gmail.com>
2023-08-16 04:11:19 +0000

    flash random score changes

--- app.py
+++ app.py
@@ -1,23 +1,42 @@
-from multiprocessing import Process, Queue
-from flask import Flask, render_template, g, request
-from teams import teams
+import json
 import pysockets as pys
+import random
+from flask import Flask, render_template, g, request
+from multiprocessing import Process, Queue
+from teams import teams
 app = Flask(__name__)
 q = Queue()
 p = Process(target=pys.run, args=(q,))
 p.start()
+SB = {}
+
+def gen_update(start=False):
+    val = 0 if start else None
+    return {
+        "nba": {team: val for team in teams["nba"]},
+        "nfl": {team: val for team in teams["nfl"]},
+        "mlb": {team: val for team in teams["mlb"]},
+    }
 
 @app.route('/')
 def index():
-    g.nba = {team: 0 for team in teams["nba"]}
-    g.nfl = {team: 0 for team in teams["nfl"]}
-    g.mlb = {team: 0 for team in teams["mlb"]}
+    global SB 
+    SB = gen_update(start=True)
+    g.sb = SB
     return render_template('index.html')
 
 @app.route('/scoreboard')
 def scoreboard():
-    return '{"nba": {"BOS": 8}}'
-
+    global SB
+    sb = SB
+    print(sb)
+    league = random.choice(list(sb))
+    team = random.choice(list(sb[league]))
+    score = sb[league][team] 
+    new_score = score + random.randint(1,7)
+    new_sb = gen_update()
+    new_sb[league][team] = new_score
+    return json.dumps(new_sb)
 
 portno=None
 with open('portno','r') as myfile:
--- static/my.js
+++ static/my.js
@@ -5,15 +5,27 @@ async function refresh() {
     const response = await fetch("/scoreboard");
     const data = await response.json();
     const nba = data.nba;
-    console.log(nba);
     for (team in nba){
       update("nba", team, nba[team]);
+    }
+    const mlb = data.mlb;
+    for (team in mlb){
+      update("mlb", team, mlb[team]);
+    }
+    const nfl = data.nfl;
+    for (team in nfl){
+      update("nfl", team, nfl[team]);
     }
 }
 
 function update(league, team, score) {
-    console.log(league + team + score)
     const team_id = league + "." + team;
-    const div = document.getElementById(team_id)
-    div.innerHTML = team + ": " + score;
+    const div = document.getElementById(team_id);
+    if (score === null) {
+        div.style.backgroundColor = "white"
+    }
+    else {
+        div.style.backgroundColor = "lightgreen"
+        div.innerHTML = team + ": " + score;
+    }
 }
