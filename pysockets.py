110e367260317d9de0437d5d73eab6d0e0816be1
Jean-Luc Chamaa <jlchamaa@gmail.com>
2024-03-25 09:44:23 -0700

    handle errors correctly.  ready for production

--- pysockets.py
+++ pysockets.py
@@ -4,19 +4,32 @@ from datetime import datetime
 import asyncio
 import json
 import logging
+import sys
 import websockets
 from handlers import all_handlers
 CBS_URI = "wss://torq.cbssports.com/torq/handler/117/7v5ku21t/websocket"
 FORMAT = '%(asctime)s %(message)s'
-logging.basicConfig(format=FORMAT)
+logging.basicConfig(stream=sys.stdout, format=FORMAT)
 log = logging.getLogger("mappy")
 log.setLevel(logging.INFO)
 
 
-def run(score_q, log_q):
+def run():
+    score_q = Queue()
+    log_q = Queue()
     my_map = Map(score_q, log_q)
-    result = asyncio.run(my_map.try_map())
+    try:
+        result = asyncio.run(my_map.try_map())
+    except (KeyboardInterrupt, asyncio.CancelledError):
+        # these exceptions are somehow uncatchable inside of run.
+        # problem described in try_map()
+        result = 4
+
     log.info(f"We recieved this result: {result}!")
+    for q in [score_q, log_q]:
+        q.close()
+
+    return result
 
 
 def destring(obj):
@@ -46,8 +59,10 @@ class Map():
             else:
                 log.error("Weird short message")
                 log.error(msg)
+
         try:
             return destring(msg)
+
         except BaseException:
             log.error("Un-de-stringable massage")
             log.error(msg)
@@ -68,25 +83,31 @@ class Map():
                     await self.attempt_all(message, ws)
                     if datetime.now().strftime("%x") != start_day:
                         # a new day has sprung!
+                        log.info("Closing because a new day has sprung!")
                         return 0
-            return 4
-        except (websockets.exceptions.ConnectionClosedOK) as e:
-            log.info("Closed for normal reasons")
-            log.info(e)
             return 1
 
-        except (
-                websockets.exceptions.InvalidStatusCode,
-                websockets.exceptions.ConnectionClosedError,
-        ) as e:
-            log.info("Closed for BAD reasons")
+        except websockets.WebSocketException as e:
+            log.info("Closed for websocket reasons")
             log.info(e)
             return 2
-        except (KeyboardInterrupt, BaseException) as e:
+
+        except BaseException as e:
             log.info("Closed for other exception")
-            log.info(e)
+            log.info(type(e))
             return 3
+
+        # except (KeyboardInterrupt, asyncio.CancelledError):
+        #     log.info("Cancelled Intentionally")
+        #     return 4
+        #
+        #  For some reason, asyncio.run() doesn't correctly handle
+        #  these errors.  If a KeyboardInterrupt happens, it will
+        #  1. hit the exception block correct
+        #  2. perform the log.info
+        #  3. hit the return line
+        #  4. force a reraise of the exception anyways!
 
 
 if __name__ == "__main__":
-    run(Queue(), Queue())
+    sys.exit(run())
